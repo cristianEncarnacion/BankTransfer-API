0c3543c2f072527ee056233dc14b49de21ee50ae
cristianEncarnacion <cencarnacion@gmail.com>
2026-06-10 16:42:48 -0400

    Implementando redis

--- src/main/java/org/acme/TransferResource.java
+++ src/main/java/org/acme/TransferResource.java
@@ -2,32 +2,38 @@ package org.acme;
 
 import java.util.List;
 
+import io.quarkus.cache.CacheInvalidateAll;
+import io.quarkus.cache.CacheResult;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 
 @Path("/transferencias")
-@RolesAllowed("User")
+@RolesAllowed("cliente_banca")
 public class TransferResource {
 
     @Inject
     TransferService transferService;
 
     @POST
+    @CacheInvalidateAll(cacheName = "transferencias-lista")
     public Transfer crear(Transfer transfer) {
         return transferService.crear(transfer);
     }
 
     @GET
+    @CacheResult(cacheName = "transferencias-lista")
     public List<Transfer> listar() {
         return transferService.listar();
     }
 
     @GET
     @Path("/{id}")
-    public Transfer listar(Long id) {
+    @CacheResult(cacheName = "transferencias-detalle")
+    public Transfer listar(@PathParam("id") Long id) {
         return transferService.listar(id);
     }
 
